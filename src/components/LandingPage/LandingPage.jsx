abed82b98a7a461cdff62a3ef5597adf261615ac
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-10-31 16:03:22 -0500

    initalized develop branch

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,6 +4,7 @@ import './LandingPage.css';
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
+import { Typography, Box, Stack, Button } from '@mui/material';
 
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome');
@@ -13,28 +14,39 @@ function LandingPage() {
     history.push('/login');
   };
 
+  const onRegistration = (event) => {
+    history.push('/registration');
+  };
+
   return (
-    <div className="container">
-      <h2>{heading}</h2>
-
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-          
-          
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
-      </div>
-    </div>
-  );
+    <Box sx={{ display: 'flex', alignContent: 'center', justifyContent: 'center', marginTop: 0 }}>
+      <Stack spacing={2} alignItems='center' justifyContent='center'>
+        
+        <Typography variant='h4' align='center'>
+          Welcome
+        </Typography>
+        
+        <Typography variant='h5' align='center'>
+          to <br /> 'logo here'
+        </Typography >
+        
+        <Button
+          variant='contained'
+          sx={{ width: '100px' }}
+          onClick={onLogin}
+        >
+          Log In
+        </Button>
+        <Button
+          variant='contained'
+          sx={{ bgcolor: 'success.main', color: 'secondary.light', width: '100px' }}
+          onClick={onRegistration}
+        >
+          Sign Up
+        </Button>
+      </Stack>
+    </Box>
+  )
 }
 
 export default LandingPage;
